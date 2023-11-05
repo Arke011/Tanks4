6abe0af41ec07420b4770c0deaf0f30b743ebff5
Arke011 <146362737+Arke011@users.noreply.github.com>
2023-11-05 17:37:43 +0200

    ok

---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    public float speed = 5;
+    public float rotateSpeed = 90;
+
+    public string vertical;
+    public string horizontal;
+    public KeyCode shootKey;
+
+    public GameObject bullet;
+
+    
+    void Update()
+    {
+        var ver = Input.GetAxis(vertical);
+        transform.position += transform.forward * speed * ver * Time.deltaTime;
+
+        var hor = Input.GetAxis(horizontal);
+        transform.Rotate(0, rotateSpeed * hor * Time.deltaTime, 0);
+
+        if (Input.GetKeyDown(shootKey))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+        }
+    }
+}
